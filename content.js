07558bc925de98b0761ab75ad6673a9ec9aedfbc
Affan Jaufar <affan.jaufar@mtcc.com.mv>
2018-04-14 20:30:26 +0500

    Hello Github

---
+++ content.js
@@ -0,0 +1,11 @@
+presentation = {
+  "title": "Hello; Welcome to Fila.",
+  "author": "Affan Jaufar",
+  "date": "12.04.2018",
+  "body": {
+    "heading": "What is Fila",
+    "content": [
+      "Fila is a very simple JSON based Presentation Tool. Since Fila used Web to Present, its by Default Platform Independent. Fila JSON file can be edited using any text editor, freeing its user from installing tools to manage the presentation.",
+      "Its Open. Its Free. Its Web. Its Yours."]
+  }
+}
