2d57dc0f79382bec9962bddf382f3bff15318ab9
Affan Jaufar <affan.jaufar@mtcc.com.mv>
2018-04-16 15:05:40 +0500

    on JS

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ presentation = {
     "1": "What is Fila",
     "1.1": [
       "Fila is a very simple JSON based Presentation Tool. Since Fila used Web to Present, its by Default Platform Independent. Fila JSON file can be edited using any text editor, freeing its user from installing tools to manage the presentation.",
-      "Its Open. Its Free. Its Web. Its Yours."]
+      "Its Open. Its Free. Its Web. Its Yours."],
+    "1.2": "This is my Second Point, it shud be shown as new Slide with Same Heading."
   }
 }
