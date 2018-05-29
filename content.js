10780f2a3c4618a34ed67d0265c8136a8cf0f3d4
Affan Jaufar <affan.jaufar@mtcc.com.mv>
2018-05-29 15:16:00 +0500

    hairloss v0.1

--- content.js
+++ content.js
@@ -1,12 +1,3 @@
 presentation = {
-  "title": "Hello; Welcome to Fila.",
-  "author": "Affan Jaufar",
-  "date": "12.04.2018",
-  "content": {
-    "1": "What is Fila",
-    "1.1": [
-      "Fila is a very simple JSON based Presentation Tool. Since Fila use Web to Present, its by Default Platform Independent. Fila JSON file can be edited using any text editor, freeing its user from installing tools to manage the presentation.",
-      "Its Open. Its Free. Its Web. Its Yours."],
-    "1.2": "This is my Second Point, it shud be shown as new Slide with Same Heading."
-  }
+  "title": "Hello; Welcome to Fila."
 }
