640eaccf6c0bfc869c906e010d67f6cb8de0f686
redhuru <redhuru@gmail.com>
2018-05-30 03:41:31 +0500

    hairloss v0.2

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
 presentation = {
-  "title": "Hello; Welcome to Fila."
+  "0": "If you we were to notice, in the past recent years, presentation has become 'minimalistic'. Instead of bullet points or large paragraphs, most presentation contain few words, more often just a single word.",
+  "0:1":"Maybe; It took us a while to realize; if you're planning to explain it; nobody likes reading it. :)",
+  "1": "But what about the Tools"
 }
