1cce0ee427337f9bbe26b57ed0d6c85a3fb5ccf1
redhuru <redhuru@gmail.com>
2018-06-01 18:10:24 +0500

    hairloss v1

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 presentation = {
-  "0": "If you we were to notice, in the past recent years, presentation has become 'minimalistic'. Instead of bullet points or large paragraphs, most presentation contain few words, more often just a single word.",
-  "0:1":"Maybe; It took us a while to realize; if you're planning to explain it; nobody likes reading it. :)",
-  "1": "But what about the Tools"
+  "0": "",
+  "0:1" : "If you we were to analyze, in the past few recent years, presentation has become 'minimalistic'. Instead of bullet points or large paragraphs, most presentation contain few words, more often just a single word.",
+  "0:2:center": "center",
+  "0:3":"Maybe; It took us a while to realize; if you're planning to explain it; nobody likes reading it. :)",
+  "1": "",
+  "1:1" : "But what about the Tools"
 }
